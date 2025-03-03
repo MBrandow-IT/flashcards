8b6f09c921f273c9e450e8ba3377e7c6cf8c3601
MBrandow-IT <masonb@net-deziner.com>
2025-03-02 18:05:08 -0700

    Added readme for others to copy the project

--- lib/db/dbSetup.sql
+++ lib/db/dbSetup.sql
@@ -1,9 +1,13 @@
 CREATE DATABASE db_GameFlashCards;
 GO
+
+-- Create the login. Please change the password to a secure one.
 CREATE LOGIN admin WITH PASSWORD = '123456'
 
 USE db_GameFlashCards;      
 
+
+-- Create the user and add it to the db_owner role.
 CREATE USER admin FOR LOGIN admin;
 ALTER ROLE db_owner ADD MEMBER admin;
 
